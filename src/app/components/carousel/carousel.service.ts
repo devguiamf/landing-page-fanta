8476f8730df1f96455d5fd150a423e338b52650a
Guilherme Almeida <guilherme.amf18@gmail.com>
2025-09-30 17:00:28 -0300

    feat: dinamic text carousel

--- src/app/components/carousel/carousel.service.ts
+++ src/app/components/carousel/carousel.service.ts
@@ -2,23 +2,53 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable(
-    {
-        providedIn: 'root'
-    }
+  {
+    providedIn: 'root'
+  }
 )
-export class CarouselService{
-    // Valor inicial do BehaviorSubject
-  private carouselIndex: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+export class CarouselService {
+  // Valor inicial do BehaviorSubject
+  private carouselcurrentIndex: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   // Observable público para os componentes se inscreverem
-  public currentCarousel: Observable<number> = this.carouselIndex.asObservable();
+  public currentCarousel: Observable<number> = this.carouselcurrentIndex.asObservable();
 
   constructor() { }
 
   // Método para atualizar o valor do BehaviorSubject
   changeCarousel(change: number): void {
-    console.log('changeCarousel',change);
-    
-    this.carouselIndex.next(change);
+    console.log('changeCarousel', change);
+
+    this.carouselcurrentIndex.next(change);
+  }
+
+  getTextByCarouselIndex(index: number) {
+    if (index == 0) {
+      return {
+        title: "Fanta Laranja",
+        description: "Um clássico que nunca sai de moda! Com seu sabor cítrico intenso e refrescante, Fanta Laranja desperta os sentidos e traz aquela sensação de energia e alegria em cada gole. Perfeita para acompanhar momentos de diversão ou simplesmente se refrescar ao longo do dia."
+      }
+    }
+
+    else if (index == 1) {
+      return {
+        title: "Fanta Uva",
+        description: "Doce, suculenta e irresistível! Fanta Uva combina a intensidade da fruta com a leveza de um refrigerante gaseificado, criando uma experiência frutada que encanta desde o primeiro gole. Ideal para quem adora sabor marcante e refrescante ao mesmo tempo."
+      }
+    }
+
+    else if (index == 2) {
+      return {
+        title: "Fanta Guárana",
+        description: "O sabor que é a cara do Brasil! Fanta Guaraná traz a energia da fruta amazônica em uma bebida leve, gaseificada e super refrescante. Perfeita para momentos de descontração, encontros com amigos ou simplesmente para se sentir renovado."
+      }
+    }
+
+    else {
+      return {
+        title: "Fanta Morango",
+        description: "Doçura, frescor e diversão em uma só bebida! Fanta Morango combina o sabor natural da fruta com a leveza do refrigerante, criando uma experiência frutada e irresistível. Cada gole é uma explosão de sabor que transforma qualquer momento em algo especial."
+      }
+    }
   }
 }
--- src/app/components/carousel/components/text-carousel.component.ts
+++ src/app/components/carousel/components/text-carousel.component.ts
@@ -7,8 +7,8 @@ import { CarouselService } from "../carousel.service";
     selector: 'app-text-carousel',
     template: `
             <div class="container px-5 py-3">
-                <h1 class="split mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
-                <p class="split mb-6 text-lg font-normal text-gray-800 lg:text-xl"> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <h1 class="split mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">{{ textProps.title }}</h1>
+                <p class="split mb-6 text-lg font-normal text-gray-800 lg:text-xl"> {{ textProps.description }}</p>
             </div>
         `,
     styles: `
@@ -27,6 +27,7 @@ export class TextCarouselComponent implements OnInit {
     private splitTween!: gsap.core.Tween;
     private splitInstance: SplitText | null = null;
     private carouselService = inject(CarouselService);
+    protected textProps!: {title: string, description: string}
 
     constructor() {
         gsap.registerPlugin(SplitText);
@@ -35,7 +36,8 @@ export class TextCarouselComponent implements OnInit {
     ngOnInit(): void {
         this.carouselService.currentCarousel.subscribe({
             next: (value: number) => {
-                this.resetAndPlayAnimation()
+                this.textProps = this.carouselService.getTextByCarouselIndex(value);
+                this.resetAndPlayAnimation();
             }
         })
     }
